943b3b91012ede137ad8dc802a261758c8c45c03
Igor Zalutski <izalutski@gmail.com>
2023-11-06 21:16:29 +0000

    Fix provider version; backend config; AZ; VPC ID; AMI ID; EC2 security group

--- prod/main.tf
+++ prod/main.tf
@@ -2,15 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.0.0"  # Use an appropriate version
+      version = "5.24.0"
     }
   }
     backend "s3" {
-    bucket = "8046b8f4c208f5bb-bucket-tfstate"
+    bucket = "digger-s3backend-quickstart-aws"              # Change if a different S3 bucket name was used for the backend 
+    /* Un-comment to use DynamoDB state locking
+    dynamodb_table = "digger-locktable-quickstart-aws"      # Change if a different DynamoDB table name was used for backend
+    */
     key    = "terraform/state"
     region = "us-east-1"
   }
-
 }
 
 provider "aws" {
@@ -27,7 +29,7 @@ resource "aws_vpc" "vpc_network" {
 resource "aws_subnet" "vpc_subnet" {
   vpc_id                  = aws_vpc.vpc_network.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2a"  
+  availability_zone       = "us-east-1a"  
   map_public_ip_on_launch = true
 
   tags = {
@@ -36,6 +38,7 @@ resource "aws_subnet" "vpc_subnet" {
 }
 
 resource "aws_security_group" "security_group" {
+  vpc_id      = aws_vpc.vpc_network.id
   name_prefix = "terraform-"
   ingress {
     from_port   = 80
@@ -46,10 +49,10 @@ resource "aws_security_group" "security_group" {
 }
 
 resource "aws_instance" "vm_instance" {
-  ami             = "ami-0b17ac7207aae009f"  #Debian 11 (bullsey AMI provided by the Debian Project https://wiki.debian.org/Cloud/AmazonEC2Image/Bullseye)
+  ami             = "ami-05c13eab67c5d8861"                   # us-east-1 Amazon Linux 2023 AMI 2023.2.20231030.1 x86_64 HVM kernel-6.1
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.vpc_subnet.id
-  security_groups = [aws_security_group.security_group.name]
+  security_groups = [aws_security_group.security_group.id]
   tags = {
     Name = "terraform-instance"
   }
