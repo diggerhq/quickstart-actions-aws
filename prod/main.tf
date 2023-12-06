bd1bf09dd207cd901fae310cad58137d172354b3
Igor Zalutski <izalutski@gmail.com>
2023-12-06 11:58:08 +0000

    Revert "Add terraform from aws quickstart + minor modifications"
    This reverts commit 6b4b9b531e64aafa4121f30c1f2189e47954240f.

--- prod/main.tf
+++ prod/main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.29.0"
+      version = "5.24.0"
     }
   }
     backend "s3" {
-    bucket = "digger-s3backend-demo-infracost-append"              # Change if a different S3 bucket name was used for the backend 
+    bucket = "digger-s3backend-quickstart-aws"              # Change if a different S3 bucket name was used for the backend 
     /* Un-comment to use DynamoDB state locking
-    dynamodb_table = "digger-locktable"      # Change if a different DynamoDB table name was used for backend
+    dynamodb_table = "digger-locktable-quickstart-aws"      # Change if a different DynamoDB table name was used for backend
     */
     key    = "terraform/state"
     region = "us-east-1"
