3ff74de4e19759a3bda6151c28c38f83d1307020
miles.lee <miles.lee@nsuslab.com>
2024-01-08 21:35:36 +0900

    test

---
+++ terraform/gp_2134/global/acm/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "nsuslab-devops-terraform-state"
+    key            = "digger/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = false
+    dynamodb_table = "nsuslab-devops-lock-table"
+    role_arn       = "arn:aws:iam::651482608654:role/assumable-cicd-role"
+  }
+}
+
+
+
+provider "aws" {
+  region = "ap-east-1"  # Replace with your desired AWS region
+}
+
+resource "aws_vpc" "vpc_network" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform-network"
+  }
+}
