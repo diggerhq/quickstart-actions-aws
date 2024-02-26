ac032adb7c6d794cdb92b59d00bd42a1267c1f04
xlrd <58804629+xlrd@users.noreply.github.com>
2024-02-26 16:26:49 +0000

    test4

--- prod/main.tf
+++ prod/main.tf
@@ -6,54 +6,17 @@ terraform {
     }
   }
     backend "s3" {
-    bucket = "digger-s3backend-quickstart-aws"              # Change if a different S3 bucket name was used for the backend 
-    /* Un-comment to use DynamoDB state locking
-    dynamodb_table = "digger-locktable-quickstart-aws"      # Change if a different DynamoDB table name was used for backend
-    */
+    bucket = "alexp-digger-s3backend-test"
     key    = "terraform/state"
-    region = "us-east-1"
+    region = "eu-west-2"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+  region = "eu-west-2"  # Replace with your desired AWS region
 }
 
-resource "aws_vpc" "vpc_network" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "terraform-network"
-  }
-}
-
-resource "aws_subnet" "vpc_subnet" {
-  vpc_id                  = aws_vpc.vpc_network.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"  
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "terraform-subnet"
-  }
-}
-
-resource "aws_security_group" "security_group" {
-  vpc_id      = aws_vpc.vpc_network.id
-  name_prefix = "terraform-"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "vm_instance" {
-  ami             = "ami-05c13eab67c5d8861"                   # us-east-1 Amazon Linux 2023 AMI 2023.2.20231030.1 x86_64 HVM kernel-6.1
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.vpc_subnet.id
-  security_groups = [aws_security_group.security_group.id]
-  tags = {
-    Name = "terraform-instance"
-  }
+resource "aws_s3_bucket" "test" {
+  bucket = "alexp-test-123"
+  acl    = "private"
 }
